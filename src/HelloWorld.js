b2a98e0e7800446b04852b10e5732fa94aeec7e2
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2023-12-01 11:18:36 +0100

    fertig

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,9 +1,57 @@
-
+import "./style/HelloWorld.css";
 
 export default function HelloWorld() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <button id="button" onClick={handleButtonClick}>Click me!</button>
+    </main>
   );
+}
+
+let clickCount = 0;
+
+function handleButtonClick(){
+  clickCount += 1;
+  if(isPrimeNumber(clickCount)){
+    changeButtonColor();
+  }
+}
+
+function changeButtonColor(){
+  const buttonElement = document.getElementById("button");
+  buttonElement.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor(){
+  const random = Math.floor(Math.random() * 4);
+  console.log(random);
+
+  const currentColor = document.getElementById("button").style.backgroundColor;
+  console.log(currentColor);
+
+  if(random === 0 && currentColor != "green"){
+    return "green";
+  }
+  else if(random === 1 && currentColor != "purple"){
+    return "purple";
+  }
+  else if(random === 2 && currentColor != "red"){
+    return "red";
+  }
+  else if(random === 3 && currentColor != "yellow"){
+    return "yellow";
+  }
+  else{
+    return "cadetblue";
+  }
+}
+
+function isPrimeNumber(number){
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+        return false;
+    }
+  }
+
+  return true;
 }
